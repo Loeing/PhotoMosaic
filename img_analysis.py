769c04617c673fe2d6214e9544a42f9a0a3dd247
Loeing <jlt81@case.edu>
2015-08-12 03:43:03 -0400

    cuts the mosaic

--- img_analysis.py
+++ img_analysis.py
@@ -1,14 +1,15 @@
-import Image
-import random
+from __future__ import print_function
+from PIL import Image
 import numpy
 
 def downsample(image, width, height):
 	downsized = image.resize((width, height), Image.ANTIALIAS)
 
 def averageColor(image):
-
 	colorTuple = [None, None, None]
 	for channel in range(3):
+		if image.mode != 'RBG':
+			image = image.convert('RGB')
 		pixels = image.getdata(band=channel)
 		values = []
 		for pixel in pixels:
@@ -17,3 +18,24 @@ def averageColor(image):
 		colorTuple[channel] = sum(values)/len(values)
 
 	return tuple(colorTuple)
+
+def cutMosaic(image, mWidth, mHeight):
+	tiles = []
+	for x in range(0,image.size[0]/mWidth):
+		for y in range(0, image.size[1]/mHeight):
+			box = (x,y,x+mWidth,y+mHeight)
+			tile = image.crop(box)
+			tiles.append(tile)
+
+	return tiles
+
+def run():
+	image = Image.open("10star.jpg")
+	mWidth = 10
+	mHeight = 10
+	tiles =  cutMosaic(image, mWidth, mHeight)
+	for tile in tiles:
+		print(averageColor(tile), tile.mode)
+		r,g,b = tile.getpixel((1,1))
+		print(r,g,b)
+	print("done")
